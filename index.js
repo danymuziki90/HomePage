ea0792e9648f83e74a84cbd942fa1d085d72ebb0
danymuziki90 <danymuziki90@gmail.com>
2023-02-06 11:47:51 +0100

    Modification 01

--- index.js
+++ index.js
@@ -1,31 +1,4 @@
-let toggle = document.querySelector('.toggle');
-let body = document.querySelector('.body');
 
-toggle.addEventListener ('click' , function) {
-    body.classList.toggle('open');
-}
-
-
-let res = 0;
-const numberOfSocks = frequencies(ar);
-
-for (let key in numberOfSocks) {
-    res += Math.floor(numberOfSocks[key] / 2);
-}
-return res;
-
-
-let valleys = 0;
-let level = 0;
-for (const step of path){
-    let prev = level;
-    level = step === 'u' ? level +1 : level -1;
-    if(
-        prev == -1 && level == 0
-    )
-    valleys++;
-}
-return valleys;
 
 
 
